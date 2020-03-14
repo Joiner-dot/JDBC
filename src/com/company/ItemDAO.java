80369a27efbff0fdd28bb7535e6e45daf9f0d801
George <egor_kuzmin22@mail.ru>
2020-03-14 11:15:58 +0300

    Feature2

--- src/com/company/ItemDAO.java
+++ src/com/company/ItemDAO.java
@@ -15,5 +15,37 @@ public class ItemDAO {
     Connectioncl connectioncl;
     Connection con;
     Statement statement;
-    
+    public Music searchbyID () throws SQLException {
+
+        ResultSet resultSet = null;
+        Music music = null;
+
+        try {
+            Class.forName ("java.sql.DriverManager");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace ();
+
+        }
+
+        try {
+            con = DriverManager.getConnection (connectioncl.getUrl (), connectioncl.getUsername (), connectioncl.getPass ());
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+
+        try {
+            statement = con.createStatement ();
+            resultSet = statement.executeQuery ("SELECT *" +
+                    "FROM `Item` " +
+                    "WHERE `id`=1");
+        } catch (SQLException e) {
+            e.printStackTrace ();
+        }
+        while (resultSet.next ()) {
+            music = new Music (resultSet.getString (2), resultSet.getString (3), resultSet.getString (4), resultSet.getInt (5), resultSet.getInt (6), resultSet.getInt (7));
+        }
+        statement.close ();
+        return music;
+
+    }
 }
